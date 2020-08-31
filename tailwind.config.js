95f39ac0da3c76a48c27e7ff556ab87a5c284e4f
Kyle Benson <kyle.benson@intercom.io>
2020-08-31 02:23:56 -0700

    migrating to Jekyll

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [`_site/**/*.html`],
+  theme: {
+    colors: {
+      alertyellow: '#ebe16f',
+      alertred: '#fbe3e4',
+      alertgreen: '#e6efc2',
+      alertblue: '#d5edf8',
+      blue: '#83d6ff',
+      bluedark: '#48768c',
+      bluedarkest: '#28424c',
+      black: '#000',
+      white: '#fff',
+      grey: '#f2ede7',
+      greytext: '#e4dacd',
+      greydark: '#bab3ae',
+      yellow: '#f5dfb2',
+      yellowdark: '#dba639',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
